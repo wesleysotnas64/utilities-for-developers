44a450c74d23fc8928527c80b98344213c6e338d
Wesley Santos <wesleysotnas64@gmail.com>
2024-07-20 11:26:48 -0300

    feat: Geração de senha aleatória de tamanho 8.

---
+++ utilities-for-developers/Controllers/GeneratorsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using utilities_for_developers.Services;
+
+namespace utilities_for_developers.Controllers
+{
+    [ApiController]
+    [Route("generator-api/")]
+    public class GeneratorsController : ControllerBase
+    {
+        public GeneratorsController() { }
+
+        [HttpGet("password")]
+        public IActionResult GeneratePassword()
+        {
+            string password = new Generators().GeneratePassword();
+            return Ok(password);
+        }
+    }
+}
---
+++ utilities-for-developers/Services/Generators.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace utilities_for_developers.Services
+{
+    public class Generators
+    {
+        public Generators() { }
+
+        public string GeneratePassword(int length = 8)
+        {
+            const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            string password = "";
+
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                password += validChars[rnd.Next(validChars.Length)];
+            }
+
+            return password;
+        }
+    }
+}
